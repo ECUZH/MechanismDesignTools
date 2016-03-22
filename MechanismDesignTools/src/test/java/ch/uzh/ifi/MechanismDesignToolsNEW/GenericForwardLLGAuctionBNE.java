3f6d9ae58f703b8ee64d93c20335263c78955ca2
dmitrymoor <dmitry.moor@yandex.ru>
2016-03-22 13:43:22 +0100

    first commit

---
+++ MechanismDesignTools/src/test/java/ch/uzh/ifi/MechanismDesignToolsNEW/GenericForwardLLGAuctionBNE.java
@@ -0,0 +1,10 @@
+package ch.uzh.ifi.MechanismDesignToolsNEW;
+
+public class GenericForwardLLGAuctionBNE {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
